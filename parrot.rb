83b75e47a5e56f0a7e66abf4c4f055a4f52565ba
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-02 18:02:01 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(name="Squawk!")
+<<<<<<< HEAD
   puts "#{name}"
   "#{name}"
+=======
+   return unless"#{name}"
+>>>>>>> 9f4f64211c8e53118310af0645fa68f5cfd0de04
    
    
   
